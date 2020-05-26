d4abc9e555276120e3975567e34b63bc4df488c0
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2020-05-26 10:52:56 +0900

    체크박스 체크 여부 확인

--- app/src/main/java/kr/co/tjoeun/sharedpreference_20200526/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/sharedpreference_20200526/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import kr.co.tjoeun.sharedpreference_20200526.databinding.ActivityMainBinding;
 
@@ -22,6 +24,21 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setupEvent() {
 
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                boolean isIdSave = binding.idSaveCheck.isChecked();
+                
+                if(isIdSave) {
+                    Toast.makeText(mContext, "아이디 저장 필요", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(mContext, "아이디 저장 안함", Toast.LENGTH_SHORT).show();
+                }
+                
+            }
+        });
     }
 
     @Override
